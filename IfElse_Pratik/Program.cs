4a3b8c96c94af4f2a29a52ed8f633642378d64da
sibel <sibel@SIBEL_PC>
2025-05-22 18:21:14 +0300

    Add project files.

---
+++ IfElse_Pratik/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("Lütfen bir sayı giriniz:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num > 10)
+{
+    Console.WriteLine("Girilen sayi 10'dan büyüktür.");
+}
+else if (num < 10)
+{
+    Console.WriteLine("Girilen sayı 10'dan küçüktür.");
+}
+else
+{
+    Console.WriteLine("Girilen sayı 10'a eşittir.");
+}
+
+if (num % 2 == 0)
+{
+    Console.WriteLine("Girilen sayı çifttir.");
+}
+else
+{
+    Console.WriteLine("Girilen sayı tektir.");
+}
+
+
